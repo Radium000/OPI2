0c52fc636bad6672cddf842a0376d963cf882c8b
Radium <litvinovich.daniil@gmail.com>
2022-12-14 15:27:41 +0300

    Добавил функцию ввода матрицы из файла

--- project/Project2.dpr
+++ project/Project2.dpr
@@ -1,17 +1,54 @@
 program Project2;
 
 {$APPTYPE CONSOLE}
-
 {$R *.res}
 
 uses
   System.SysUtils;
 
+type
+  TArray = array [1 .. 10, 1 .. 10] of integer;
+
+function readMatrixFromFile(const fileName: string): TArray;
+var
+  f: TextFile;
+  s: string;
+  i, j: integer;
 begin
-  try
-    { TODO -oUser -cConsole Main : Insert code here }
-  except
-    on E: Exception do
-      Writeln(E.ClassName, ': ', E.Message);
+  for i := 1 to 10 do
+    for j := 1 to 10 do
+      Result[i, j] := 0;
+
+  AssignFile(f, fileName);
+  Reset(f);
+  i := 1;
+  while not Eof(f) do
+  begin
+    ReadLn(f, s);
+    s := trim(s);
+    if (pos('Ê', s) <> 0) and (pos('Ì', s) <> 0) then
+    begin
+      j := 1;
+      while (j < Length(s)) do
+      begin
+        if s[j] = 'Ì' then
+          Result[i, j] := 0
+        else if s[j] = 'Ê' then
+          Result[i, j] := -1
+        else
+        begin
+          delete(s, j, 1);
+          dec(j);
+        end;
+        Inc(j);
+      end;
+
+      Inc(i);
+    end;
   end;
+  CloseFile(f);
+end;
+
+begin
+
 end.
